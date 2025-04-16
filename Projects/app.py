12dc93dc006870a5265b1ada1ce264c63b98ea96
geetathakur13 <geetathakurr13@gmail.com>
2025-04-16 19:17:51 +0530

    Update app.py

--- Projects/app.py
+++ Projects/app.py
@@ -42,13 +42,12 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 
 # Load Models Safely
 models = {
-    'diabetes': pickle.load(open('Models/diabetes_model.sav', 'rb')),
-    'heart_disease': pickle.load(open('Models/heart_disease_model.sav', 'rb')),
-    'parkinsons': pickle.load(open('Models/parkinsons_model.sav', 'rb')),
-    'lung_cancer': pickle.load(open('Models/lungs_disease_model.sav', 'rb')),
-    'thyroid': pickle.load(open('Models/Thyroid_model.sav', 'rb'))
+    'diabetes': pickle.load(open('Projects/Models/diabetes_model.sav', 'rb')),
+    'heart_disease': pickle.load(open('Projects/Models/heart_disease_model.sav', 'rb')),
+    'parkinsons': pickle.load(open('Projects/Models/parkinsons_model.sav', 'rb')),
+    'lung_cancer': pickle.load(open('Projects/Models/lungs_disease_model.sav', 'rb')),
+    'thyroid': pickle.load(open('Projects/Models/Thyroid_model.sav', 'rb'))
 }
-
 
 # Dropdown for Disease Selection
 selected = st.selectbox(
